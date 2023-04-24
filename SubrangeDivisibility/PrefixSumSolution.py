78d17e7e781d8c259f2a0dd0aa25bf0f22b81a4a
Osama Khallouf <50551721+Exeedo@users.noreply.github.com>
2023-04-24 14:56:43 +0300

    Create PrefixSumSolution.py

---
+++ SubrangeDivisibility/PrefixSumSolution.py
@@ -0,0 +1,12 @@
+X = input()
+N = len(X)
+L = [int(digit) for digit in X]
+S = [0 for _ in range(N)]
+for i in range(N):
+    S[i] = S[i-1] + L[i]
+
+Q = int(input())
+for _ in range(Q):
+    L, R = [int(x) for x in input().split()]
+    s = S[R] if L == 0 else S[R] - S[L-1]
+    print("YES" if s % 3 == 0 else "NO")
